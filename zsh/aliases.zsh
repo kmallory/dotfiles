6d5da97b49b231eb9bf168bfaf00ce41b287668a
Kevin Mallory <kevin_mallory@intuit.com>
2012-11-15 11:15:43 -0800

    my changes

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,1 +1,43 @@
 alias reload!='. ~/.zshrc'
+
+alias dev="cd ~/Documents/p4/SEPG" 
+alias gdev="cd ~/Documents/git"
+alias idev="cd ~/Documents/icode"
+alias gicode="ssh pharos_priv_user@pdevicows100.corp.intuit.net"
+alias gicodedb="ssh kmallory@pdevcttdb101.corp.intuit.net"
+alias gsonar="ssh pharos_priv_user@pdevsonao100.corp.intuit.net"
+alias gfe="ssh kmallory@pprdjraap300.corp.intuit.net"
+alias gfea="ssh kmallory@pprdjaaws300.corp.intuit.net"
+alias gsdev="ssh kmallory@pdevcttwd100.corp.intuit.net"
+
+alias gjirapilotdb="ssh pharos_priv_user@pppdjradb100.corp.intuit.net"
+alias gicoded="ssh kmallory@pdevcttao105.corp.intuit.net"
+alias gicodek="ssh kmallory@pdevcttwd102.corp.intuit.net"
+alias gicodekdb="ssh kmallory@pdevcttwd103.corp.intuit.net"
+alias gstashd="ssh kmallory@pdevcttwd100.corp.intuit.net" 
+
+alias reset-vmware='sudo /Library/Application\ Support/VMware\ Fusion/boot.sh --restart'
+alias gjiradev="ssh kmallory@pppdjrawa100.corp.intuit.net"
+
+
+
+alias giq="ssh kmallory@pprdicdws302.corp.intuit.net"
+alias giaq="ssh kmallory@pprdicdws301.corp.intuit.net"
+alias gsdbq="ssh kmallory@pprdicddb300.corp.intuit.net"
+alias gidbq="ssh kmallory@pprdicddb301.corp.intuit.net"
+alias gsq="ssh kmallory@pprdicdws300.corp.intuit.net" 
+
+alias gil="ssh kmallory@pprdicdws402.corp.intuit.net"
+alias gial="ssh kmallory@pprdicdws401.corp.intuit.net"
+alias gidbl="ssh kmallory@pprdicddb401.corp.intuit.net"
+alias gsdbl="ssh kmallory@pprdicddb400.corp.intuit.net"
+alias gsl="ssh kmallory@pprdicdws400.corp.intuit.net" 
+
+alias imetrics="ssh kmallory@pdevcttao10a.corp.intuit.net" 
+
+alias top='top -ocpu'
+alias ls='ls -F'
+set color 
+set autolist 
+set colorcat 
+
