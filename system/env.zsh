24062cb01413786ad08edb821f979e524f2f0bf1
Kevin Mallory <kevin_mallory@intuit.com>
2012-11-15 11:19:18 -0800

    my changes

--- system/env.zsh
+++ system/env.zsh
@@ -1,1 +1,7 @@
 export EDITOR='subl'
+
+export CLICOLOR
+export P4PORT=sepg-scm-prd01.bos.intuit.net:1666
+export P4CLIENT=kevin-mac
+#export EDITOR vim
+export ICODE_SCRIPTS=/Users/kmallory/Documents/icode/scripts/
