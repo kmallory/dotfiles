54ed467768d2395bef2085103ea067b2ea10987e
kevin mallory <kmallory@gourmetgeeks.org>
2014-10-08 04:55:41 -0700

    fish config

--- config-symlink/fish/config.fish
+++ config-symlink/fish/config.fish
@@ -1,29 +1,30 @@
- set fish_greeting ""
- function ls
+function mvi 
+/usr/bin/vi 
+end
+function ls
            /bin/ls -FG $argv
  end
- #function fish_prompt
- #        ~/powerline-shell.py $status --shell bare ^/dev/null
- #            end
+ function  xlinux 
+		 ssh linux Xnest :1 -geometry 1280x810 -query localhost 
+ end 
+alias gvim='/usr/local/bin/gvim' 
+alias vim='/usr/local/bin/vim'
+alias view='/usr/local/bin/mvim -M'
+alias eclim='/Applications/eclipse/eclipse/plugins/org.eclim_2.4.0/bin/eclim'
 alias dog='cd ~/Downloads/pwd'
-alias dogr='cd /Volumes/c\$/pwd'
+alias dogr='cd /Volumes/pwdhld-devel'
+alias dogprod='cd /Volumes/pwd'
 alias gh='cd ~/Documents/github'
-function gvim 
-        /Applications/Shareware/MacVim.app/Contents/MacOS/MacVim  $argv
-end 
-function vi 
-			/Applications/Shareware/MacVim.app/Contents/MacOS/Vim $argv
-end
-
-alias vim='/Applications/Shareware/MacVim.app/Contents/MacOS/Vim'
-alias view='/Applications/Shareware/MacVim.app/Contents/MacOS/Vim'
 #
 set PATH /usr/local/bin $PATH /Users/kmallory/bin /Users/kmallory/Documents/github/powerline/scripts ~/.rvm/bin
-set GIT_EDITOR /Applications/Shareware/MacVim.app/Contents/MacOS/Vim
-set EDITOR /Applications/Shareware/MacVim.app/Contents/MacOS/Vim
-set VISUAL /Applications/Shareware/MacVim.app/Contents/MacOS/Vim
+set GIT_EDITOR /usr/local/bin/vim 
+set EDITOR /usr/local/bin/vim 
+set VISUAL /usr/local/bin/vim 
 . /Users/kmallory/Documents/github/powerline/powerline/bindings/fish/powerline-setup.fish
 set ECLIPSE_HOME /Applications/eclipse/eclipse
 set fish_function_path $fish_function_path "/Users/kmallory/Documents/github/powerline/powerline/bindings/fish"
 powerline-setup
+ set fish_greeting ""
+
+set NODE_ENV DEVELOPMENT
 
