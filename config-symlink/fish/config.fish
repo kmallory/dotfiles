ac158a6fd4a879724efa758fad5f013626a02649
Kevin Mallory <kmallory@gourmetgeeks.org>
2014-10-10 16:24:28 -0700

    oy!

--- config-symlink/fish/config.fish
+++ config-symlink/fish/config.fish
@@ -1,3 +1,4 @@
+set POWERLINE_COMMAND powerline-client
 function mvi 
 /usr/bin/vi 
 end
@@ -8,23 +9,23 @@ function ls
 		 ssh linux Xnest :1 -geometry 1280x810 -query localhost 
  end 
 alias gvim='/usr/local/bin/gvim' 
-alias vim='/usr/local/bin/vim'
 alias view='/usr/local/bin/mvim -M'
+alias vim='/usr/local/bin/vi'
 alias eclim='/Applications/eclipse/eclipse/plugins/org.eclim_2.4.0/bin/eclim'
 alias dog='cd ~/Downloads/pwd'
 alias dogr='cd /Volumes/pwdhld-devel'
 alias dogprod='cd /Volumes/pwd'
 alias gh='cd ~/Documents/github'
 #
-set PATH /usr/local/bin $PATH /Users/kmallory/bin /Users/kmallory/Documents/github/powerline/scripts ~/.rvm/bin
+set PATH /usr/local/bin $PATH /Users/kmallory/bin /Users/kmallory/dotfiles/vim/vim.symlink/bundle/powerline ~/.rvm/bin
 set GIT_EDITOR /usr/local/bin/vim 
 set EDITOR /usr/local/bin/vim 
 set VISUAL /usr/local/bin/vim 
 . /Users/kmallory/Documents/github/powerline/powerline/bindings/fish/powerline-setup.fish
 set ECLIPSE_HOME /Applications/eclipse/eclipse
-set fish_function_path $fish_function_path "/Users/kmallory/Documents/github/powerline/powerline/bindings/fish"
+#set fish_function_path $fish_function_path "/Users/kmallory/dotfiles/vim/vim.symlink/bundle/powerline/powerline/bindings/fish"
 powerline-setup
- set fish_greeting ""
+set fish_greeting ""
 
 set NODE_ENV DEVELOPMENT
 
